f5f0fbc281253b895123c2a995375b8a693786e0
pjthin <pierrejean.thin@gmail.com>
2016-05-01 16:24:54 +0200

    clean up

--- vertx-starter/vertx-services/src/main/java/fr/pjthin/vertx/service/Launcher.java
+++ vertx-starter/vertx-services/src/main/java/fr/pjthin/vertx/service/Launcher.java
@@ -3,15 +3,8 @@ package fr.pjthin.vertx.service;
 import fr.pjthin.vertx.service.container.ServiceContainer;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.json.JsonObject;
 import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +12,6 @@ public class Launcher {
     private static final Logger LOGGER = LoggerFactory.getLogger(Launcher.class);
 
     public static void main(String[] args) {
-        // TODO extract configuration of manager into a file
         VertxOptions vertxOptions = new VertxOptions(
                 ConfigurationUtils.loadJsonFromClassPath(ConfigurationUtils.CLUSTER_PATH))
                 .setClusterManager(new HazelcastClusterManager());
@@ -47,21 +39,6 @@ public class Launcher {
                 LOGGER.error("Failed starting services: " + complete.cause().getMessage(), complete.cause());
             }
         });
-    }
-
-    private static void loadConfiguration() {
-        try {
-            Path path = Paths.get(Launcher.class.getResource("/mongodb.json").toURI());
-            String json = FileUtils.readFileToString(path.toFile(), "UTF-8");
-            JsonObject config = new JsonObject(json);
-            config.toString();
-        } catch (URISyntaxException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
     }
 
 }
