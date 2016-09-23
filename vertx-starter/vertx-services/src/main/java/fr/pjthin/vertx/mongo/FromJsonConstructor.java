7ac99bc09883bfc088735dbd000bba446c4b32c1
Pierre-Jean.Thin <Pierre-Jean.Thin@LIN0108238.CMB.PLATINE.ORG>
2016-09-23 15:00:45 +0200

    change pom and modify exception handling

--- vertx-starter/vertx-services/src/main/java/fr/pjthin/vertx/mongo/FromJsonConstructor.java
+++ vertx-starter/vertx-services/src/main/java/fr/pjthin/vertx/mongo/FromJsonConstructor.java
@@ -1,12 +1,12 @@
 package fr.pjthin.vertx.mongo;
 
-import io.vertx.core.json.JsonObject;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Stream;
+
+import io.vertx.core.json.JsonObject;
 
 class FromJsonConstructor<T> implements Function<JsonObject, T> {
 
@@ -15,9 +15,9 @@ class FromJsonConstructor<T> implements Function<JsonObject, T> {
     @SuppressWarnings("unchecked")
     public FromJsonConstructor(Class<T> klass) {
         Optional<Constructor<?>> constructor = getJsonConstructor(klass);
-        this.constructor = (Constructor<T>) constructor.orElseThrow(() -> {
-            throw new RuntimeException(String.format("Fail to find constructor for %s", klass));
-        });
+        this.constructor = (Constructor<T>) constructor.orElseThrow(() -> 
+            new RuntimeException(String.format("Fail to find constructor for %s", klass))
+        );
     }
 
     private Optional<Constructor<?>> getJsonConstructor(Class<T> persistentClass) {
--- vertx-starter/vertx-services/src/main/java/fr/pjthin/vertx/mongo/ToJsonMethod.java
+++ vertx-starter/vertx-services/src/main/java/fr/pjthin/vertx/mongo/ToJsonMethod.java
@@ -1,7 +1,5 @@
 package fr.pjthin.vertx.mongo;
 
-import io.vertx.core.json.JsonObject;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -9,16 +7,18 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+import io.vertx.core.json.JsonObject;
+
 class ToJsonMethod<T> implements Function<T, JsonObject> {
 
     private Method method;
 
     public ToJsonMethod(Class<T> klass) {
         Optional<Method> method = getToJsonMethod(klass);
-        this.method = method.orElseThrow(() -> {
-            throw new RuntimeException(String
-                    .format("Fail to find method type 'JsonObject methodName()' for %s", klass));
-        });
+        this.method = method.orElseThrow(() -> 
+            new RuntimeException(String
+                    .format("Fail to find method type 'JsonObject methodName()' for %s", klass))
+        );
     }
 
     private Optional<Method> getToJsonMethod(Class<T> klass) {
